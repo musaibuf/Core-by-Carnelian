0eb2c55678116ceb1f147ebad99b68d9ed0f4828
musaibuf <97942670+musaibuf@users.noreply.github.com>
2026-04-16 10:43:41 +0500

    made database connect to backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -14,12 +14,9 @@ app.use(express.json()); // Parses incoming JSON requests
 
 // Database connection
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
 });
+
 
 // Test DB Connection
 pool.connect()
